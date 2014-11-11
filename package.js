6959986347d50e35feaa7f1b32d70018170d468d
Eric Dobbertin <eric@dairystatedesigns.com>
2014-11-10 23:38:17 -0600

    export

--- package.js
+++ package.js
@@ -9,6 +9,8 @@ Package.onUse(function(api) {
   api.use('underscore@1.0.1', ['client']);
   api.use('tracker@1.0.3', ['client']);
   api.use('reload@1.1.1', ['client', 'server'], {weak: true});
+
+  api.export('ReactiveClock');
   
   api.add_files(['clock.js'], 'client');
 });
